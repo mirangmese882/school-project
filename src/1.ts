40ee3b863d91967179e36e93bf86cc0db5e96278
mirangmese882 <mirangmese882@gmail.com>
2025-03-06 08:20:48 +0000

    Add 1.ts

---
+++ src/1.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'My App';
+  userForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private http: HttpClient, private router: Router) { }
+
+  ngOnInit() {
+    this.userForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  onSubmit() {
+    if (this.userForm.invalid) {
+      return;
+    }
+
+    this.http.post('https://example.com/api/users', this.userForm.value).subscribe(() => {
+      this.router.navigate(['']);
+    });
+  }
+}
