20a0c0d43cde85d2158525b8fedc24390ab35366
mirangmese882 <mirangmese882@gmail.com>
2025-03-09 08:52:07 +0000

    Add 4.ts

---
+++ src/4.ts
@@ -0,0 +1,11 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class UserService {
+  constructor(private http: HttpClient) {}
+
+  getUsers() {
+    return this.http.get('https://jsonplaceholder.typicode.com/users');
+  }
+}
