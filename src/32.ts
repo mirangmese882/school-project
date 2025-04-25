76e72dd0c25e83c9a2c1cfdf1482e8285a13c487
mirangmese882 <mirangmese882@gmail.com>
2025-04-25 04:37:05 +0000

    Add 32.ts

---
+++ src/32.ts
@@ -0,0 +1,9 @@
+let name = "World";
+console.log(name);
+
+let age = 20;
+if (age > 18) {
+  console.log("You are old enough to vote.");
+} else {
+  console.log("You are too young for voting.");
+}
